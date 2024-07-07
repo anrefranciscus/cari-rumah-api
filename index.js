9d542ef54e3fe5ed0602dcaaa13b2f539105cf9b
anrefranciscus <anrefranciscus@gmail.com>
2024-07-07 20:48:27 +0700

    create signup for api route

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js"
+import authRoutes from "./routes/auth.route.js"
 
 dotenv.config();
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -12,7 +15,11 @@ mongoose
     console.log(err);
   });
 const app = express();
+
+app.use(express.json())
 app.listen(3001,() => {
   console.log("server running on port 3001");
 });
 
+app.use("/api/user", userRoutes)
+app.use("/api/auth", authRoutes)
