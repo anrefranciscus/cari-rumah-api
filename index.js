3c47fac1de26747f71d2e86a33c5b070bb4e8464
anrefranciscus <anrefranciscus@gmail.com>
2024-07-07 22:09:27 +0700

    create signin api route

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ mongoose
 const app = express();
 
 app.use(express.json());
-app.listen(3001, () => {
-  console.log("server running on port 3001");
+app.listen(3000, () => {
+  console.log("server running on port 3000");
 });
 
 app.use("/api/user", userRoutes);
